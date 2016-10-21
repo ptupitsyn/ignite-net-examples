daabc3b5bd710a2379782ee6484f97dc25b9d737
Pavel Tupitsyn <ptupitsyn@apache.org>
2016-10-21 14:10:00 +0300

    Code cleanup

--- EFCacheStore/IgniteEFCacheStore/BloggingContext.cs
+++ EFCacheStore/IgniteEFCacheStore/BloggingContext.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 
+// ReSharper disable UnusedAutoPropertyAccessor.Global
 namespace IgniteEFCacheStore
 {
     public class BloggingContext : DbContext
