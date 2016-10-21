b510f0d0d627e3cf25da6c9de5f7ab2ce83d701c
Pavel Tupitsyn <ptupitsyn@apache.org>
2016-10-21 13:17:44 +0300

    Database initialization

--- EFCacheStore/IgniteEFCacheStore/Program.cs
+++ EFCacheStore/IgniteEFCacheStore/Program.cs
@@ -1,24 +1,48 @@
 ﻿using System;
-using System.Linq;
-using Apache.Ignite.Core;
+using System.Collections.Generic;
+using System.IO;
 
 namespace IgniteEFCacheStore
 {
     public static class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             //Ignition.StartFromApplicationConfiguration();
 
+            InitializeDb();
+        }
+
+        private static void InitializeDb()
+        {
             using (var ctx = new BloggingContext())
             {
-                ctx.Database.CreateIfNotExists();
+                // ReSharper disable once AssignNullToNotNullAttribute
+                var dataSource = Path.GetFullPath(ctx.Database.Connection.DataSource);
 
-                ctx.Blogs.Add(new Blog {Name = "MyBlog1"});
+                if (ctx.Database.CreateIfNotExists())
+                {
+                    var blog = new Blog
+                    {
+                        Name = "Ignite Blog",
+                        Posts = new List<Post>
+                        {
+                            new Post
+                            {
+                                Title = "Getting Started With Ignite.NET",
+                                Content = "Refer to https://ptupitsyn.github.io/Getting-Started-With-Apache-Ignite-Net/"
+                            }
+                        }
+                    };
 
-                ctx.SaveChanges();
+                    ctx.Blogs.Add(blog);
 
-                Console.WriteLine(ctx.Blogs.Count());
+                    Console.WriteLine("Database created at {0} with {1} entities.", dataSource, ctx.SaveChanges());
+                }
+                else
+                {
+                    Console.WriteLine("Database already exists at {0}.", dataSource);
+                }
             }
         }
     }
