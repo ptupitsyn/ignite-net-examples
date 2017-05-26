0cff297910157bdaa9172cb725a165536d5d3c64
Pavel Tupitsyn <ptupitsyn@apache.org>
2017-05-26 19:16:11 +0300

    wip

--- AdoNetCacheStore/AdoNetCacheStore/Program.cs
+++ AdoNetCacheStore/AdoNetCacheStore/Program.cs
@@ -1,4 +1,5 @@
-﻿using Apache.Ignite.Core;
+﻿using System;
+using Apache.Ignite.Core;
 using Apache.Ignite.Core.Binary;
 using Apache.Ignite.Core.Cache;
 using Apache.Ignite.Core.Cache.Configuration;
@@ -30,7 +31,18 @@ namespace AdoNetCacheStore
                     .SetIntField("Power", 600)
                     .Build();
 
-                cars[1] = car;
+                // Put an entry to Ignite cache, this causes AdoNetCacheStore.Write call.
+                cars.Put(1, car);
+
+                // Remove an entry from Ignite cache, but not from cache store.
+                Console.WriteLine("Cache size before Clear: " + cars.GetSize());
+                cars.Clear();
+                Console.WriteLine("Cache size after Clear: " + cars.GetSize());
+
+                // Get an entry by key, which delegates to AdoNetCacheStore.Load.
+                Console.WriteLine("Requesting key from Ignite cache...");
+                IBinaryObject carFromStore = cars.Get(1);
+                Console.WriteLine("Entry from cache store: " + carFromStore);
             }
         }
     }
