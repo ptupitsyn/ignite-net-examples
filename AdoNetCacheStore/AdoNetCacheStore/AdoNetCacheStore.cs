d2b5acc33d1b2158a8b03c1ac9812d54181d4af6
Pavel Tupitsyn <ptupitsyn@apache.org>
2017-05-26 19:08:35 +0300

    wip

--- AdoNetCacheStore/AdoNetCacheStore/AdoNetCacheStore.cs
+++ AdoNetCacheStore/AdoNetCacheStore/AdoNetCacheStore.cs
@@ -2,56 +2,23 @@
 using System.Collections.Generic;
 using System.Data.SqlServerCe;
 using System.IO;
+using System.Linq;
+using Apache.Ignite.Core;
+using Apache.Ignite.Core.Binary;
 using Apache.Ignite.Core.Cache.Store;
 using Apache.Ignite.Core.Common;
+using Apache.Ignite.Core.Resource;
 
 namespace AdoNetCacheStore
 {
-    public class AdoNetCacheStore : ICacheStore<int, string>
+    public class AdoNetCacheStore : ICacheStore<int, IBinaryObject>
     {
         public const string DbName = "cars.db";
 
         public const string ConnectionString = "DataSource=" + DbName;
 
-        public void LoadCache(Action<int, string> act, params object[] args)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string Load(int key)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<KeyValuePair<int, string>> LoadAll(IEnumerable<int> keys)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Write(int key, string val)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void WriteAll(IEnumerable<KeyValuePair<int, string>> entries)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Delete(int key)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void DeleteAll(IEnumerable<int> keys)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void SessionEnd(bool commit)
-        {
-            throw new NotImplementedException();
-        }
+        [InstanceResource]
+        private IIgnite Ignite { get; set; }
 
         public static void InitializeDb()
         {
@@ -72,6 +39,72 @@ namespace AdoNetCacheStore
             }
         }
 
+        public void LoadCache(Action<int, IBinaryObject> act, params object[] args)
+        {
+            // No-op.
+        }
+
+        public IBinaryObject Load(int key)
+        {
+            using (var conn = new SqlCeConnection(ConnectionString))
+            {
+                using (var cmd = new SqlCeCommand(@"SELECT Name, Power FROM Cars WHERE Id = @id", conn))
+                {
+                    cmd.Parameters.AddWithValue("@id", key);
+
+                    conn.Open();
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return Ignite.GetBinary()
+                                .GetBuilder("Car")
+                                .SetStringField("Name", reader.GetString(0))
+                                .SetIntField("Power", reader.GetInt32(1))
+                                .Build();
+                        }
+                        
+                        return null;
+                    }
+                }
+            }
+        }
+
+        public IEnumerable<KeyValuePair<int, IBinaryObject>> LoadAll(IEnumerable<int> keys)
+        {
+            return keys.ToDictionary(x => x, Load);
+        }
+
+        public void Write(int key, IBinaryObject val)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteAll(IEnumerable<KeyValuePair<int, IBinaryObject>> entries)
+        {
+            foreach (var pair in entries)
+            {
+                Write(pair.Key, pair.Value);
+            }
+        }
+
+        public void Delete(int key)
+        {
+            // TODO
+        }
+
+        public void DeleteAll(IEnumerable<int> keys)
+        {
+            foreach (var key in keys)
+            {
+                Delete(key);
+            }
+        }
+
+        public void SessionEnd(bool commit)
+        {
+            // No-op.
+        }
     }
 
     public class AdoNetCacheStoreFactory : IFactory<AdoNetCacheStore>
