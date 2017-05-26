51c9e640c55d1ee22a101cdcf309cb2f588b0758
Pavel Tupitsyn <ptupitsyn@apache.org>
2017-05-26 18:59:22 +0300

    wip

---
+++ AdoNetCacheStore/AdoNetCacheStore/AdoNetCacheStore.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Apache.Ignite.Core.Cache.Store;
+using Apache.Ignite.Core.Common;
+
+namespace AdoNetCacheStore
+{
+    public class AdoNetCacheStore : ICacheStore<int, string>
+    {
+        public void LoadCache(Action<int, string> act, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Load(int key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<KeyValuePair<int, string>> LoadAll(IEnumerable<int> keys)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write(int key, string val)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteAll(IEnumerable<KeyValuePair<int, string>> entries)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAll(IEnumerable<int> keys)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SessionEnd(bool commit)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class AdoNetCacheStoreFactory : IFactory<AdoNetCacheStore>
+    {
+        public AdoNetCacheStore CreateInstance()
+        {
+            return new AdoNetCacheStore();
+        }
+    }
+}
--- AdoNetCacheStore/AdoNetCacheStore/Program.cs
+++ AdoNetCacheStore/AdoNetCacheStore/Program.cs
@@ -1,12 +1,61 @@
-﻿using Apache.Ignite.Core;
+﻿using System.ComponentModel;
+using System.Data.SqlServerCe;
+using System.IO;
+using Apache.Ignite.Core;
+using Apache.Ignite.Core.Binary;
+using Apache.Ignite.Core.Cache;
+using Apache.Ignite.Core.Cache.Configuration;
 
 namespace AdoNetCacheStore
 {
     class Program
     {
+        public const string DbName = "cars.db";
+        public const string ConnectionString = "DataSource=" + DbName;
+
         static void Main()
         {
-            Ignition.StartFromApplicationConfiguration();
+            InitializeDb();
+
+            using (var ignite = Ignition.StartFromApplicationConfiguration())
+            {
+                var cacheCfg = new CacheConfiguration
+                {
+                    Name = "cars",
+                    KeepBinaryInStore = true,
+                    CacheStoreFactory = new AdoNetCacheStoreFactory()
+                };
+
+                ICache<int, IBinaryObject> cars = ignite.GetOrCreateCache<int, object>(cacheCfg).WithKeepBinary<int, IBinaryObject>();
+
+                IBinaryObject car = ignite.GetBinary()
+                    .GetBuilder("Car")
+                    .SetStringField("Name", "Honda NSX")
+                    .SetIntField("Power", 600)
+                    .Build();
+
+                cars[1] = car;
+            }
+
+        }
+
+        private static void InitializeDb()
+        {
+            File.Delete(DbName);
+
+            using (var engine = new SqlCeEngine(ConnectionString))
+            {
+                engine.CreateDatabase();
+            }
+
+            using (var conn = new SqlCeConnection(ConnectionString))
+            {
+                using (var cmd = new SqlCeCommand(@"CREATE TABLE Cars (ID int, Name NVARCHAR(200), Power int)", conn))
+                {
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
