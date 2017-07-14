35673718de7af6bee9886c0a5a2cf878103ad082
Pavel Tupitsyn <ptupitsyn@apache.org>
2017-07-14 13:06:53 +0300

    Solution added

---
+++ Plugin/CSharp/IgnitePlugin/Program.cs
@@ -0,0 +1,12 @@
+﻿using Apache.Ignite.Core;
+
+namespace IgnitePlugin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ignition.Start();
+        }
+    }
+}
