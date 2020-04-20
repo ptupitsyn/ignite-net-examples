41428be320875d6e8a5a3675a2b94b54c87b5c1b
Pavel Tupitsyn <ptupitsyn@apache.org>
2020-04-20 21:41:43 +0300

    wip

--- CacheNodeFilter/Program.cs
+++ CacheNodeFilter/Program.cs
@@ -7,7 +7,26 @@ namespace CacheNodeFilter
     {
         static void Main(string[] args)
         {
-            Ignition.Start("ignite-spring-config.xml");
+            // Start Ignite server with Users.
+            var userServer = Ignition.Start(
+                new IgniteConfiguration {SpringConfigUrl = "user-node-config.xml"});
+
+            var userNode = userServer.GetCluster().GetLocalNode();
+
+            // Start Ignite server with Companies.
+            var companyServer = Ignition.Start(
+                new IgniteConfiguration {SpringConfigUrl = "company-node-config.xml"});
+
+            var companyNode = companyServer.GetCluster().GetLocalNode();
+
+            // Start Client node to demonstrate data distribution.
+            var client = Ignition.Start(
+                new IgniteConfiguration {ClientMode = true, Localhost = "127.0.0.1"});
+
+            var userPartitionsOnUserNode = client.GetAffinity("user").GetAllPartitions(userNode).Length;
+            var userPartitionsOnCompanyNode = client.GetAffinity("user").GetAllPartitions(userNode).Length;
+            
+            Console.WriteLine($"User partitions on user node: {userPartitionsOnUserNode}, on company node: {userPartitionsOnCompanyNode}");
         }
     }
 }
