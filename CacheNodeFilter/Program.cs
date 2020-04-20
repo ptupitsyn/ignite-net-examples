2dc21e7efa9c4f41fec3f5c336d48f8516a27849
Pavel Tupitsyn <ptupitsyn@apache.org>
2020-04-20 21:44:18 +0300

    Add CacheNodeFilter example

---
+++ CacheNodeFilter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Apache.Ignite.Core;
+
+namespace CacheNodeFilter
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Start Ignite server with Users.
+            var userServer = Ignition.Start(
+                new IgniteConfiguration {SpringConfigUrl = "user-node-config.xml"});
+
+            var userNode = userServer.GetCluster().GetLocalNode();
+
+            // Start Ignite server with Companies.
+            var companyServer = Ignition.Start(
+                new IgniteConfiguration {SpringConfigUrl = "company-node-config.xml"});
+
+            var companyNode = companyServer.GetCluster().GetLocalNode();
+
+            // Start Client node to demonstrate data distribution.
+            var client = Ignition.Start(
+                new IgniteConfiguration {ClientMode = true, Localhost = "127.0.0.1"});
+
+            var userPartitionsOnUserNode = client.GetAffinity("user").GetAllPartitions(userNode).Length;
+            var userPartitionsOnCompanyNode = client.GetAffinity("user").GetAllPartitions(companyNode).Length;
+            
+            // All user cache data (partitions) are on user node, none on company node:
+            Console.WriteLine($"User partitions on user node: {userPartitionsOnUserNode}, on company node: {userPartitionsOnCompanyNode}");
+        }
+    }
+}
